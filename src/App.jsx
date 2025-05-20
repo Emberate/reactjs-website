4bc3c827341a80882188775a687db4c636a5b19c
@swastik1456 <swastik@grktraining.com>
2025-05-20 16:54:23 +0530

    menu file added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Home from './components/Home'
 import Page from './components/Page'
+import Menu from './components/Menu'
 function App() {
   return (
     <div>
       <Home/>
       <Page/>
+      <Menu/>
     </div>
   )
 }
